cd7c3d4534ea1514d861a9e9052459fe015b0a61
nyanwin myo <nyanwinmyo@onenex.co>
2025-08-13 00:50:21 +0630

    added user

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Resources\Users\Schemas;
 
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
 class UserForm
@@ -10,7 +12,18 @@ class UserForm
     {
         return $schema
             ->components([
-                //
+                TextInput::make('name')->required()->maxLength(255),
+                TextInput::make('email')->required()->maxLength(255),
+                Select::make('role_id')
+                    ->label('Role')
+                    ->relationship('role', 'name')
+                    ->required(),
+                TextInput::make('password')
+                    ->password()
+                    ->required(fn(string $context) => $context === 'create')
+                    ->dehydrated(fn($state) => filled($state))
+                    ->maxLength(255),
+
             ]);
     }
 }
--- app/Filament/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Users\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class UsersTable
@@ -13,7 +14,21 @@ class UsersTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('serial_no')
+                    ->label('No.')
+                    ->rowIndex(),
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('email')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('role.name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->searchable()
+                    ->sortable(),
             ])
             ->filters([
                 //
